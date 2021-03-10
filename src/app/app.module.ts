7ab331cbf581734476d74384c4736e8f126583d1
Trijit Goswami <goswami.trijit@gmail.com>
2021-03-10 07:16:13 +0530

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BodyComponent } from './body/body.component';
+import { ThumbnailComponent } from './body/thumbnail/thumbnail.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatIconModule } from '@angular/material/icon';
+import { AvatarModule } from 'ngx-avatar';
+import {MatButtonModule} from '@angular/material/button';
+
+import { TaskItemsComponent } from './sidebar/task-items/task-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BodyComponent,
+    ThumbnailComponent,
+    TaskItemsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatIconModule,
+    AvatarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
